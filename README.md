dc5b6534a5023bf20706d2f6ed55ef2eae0182e3
hetiantian <969795191@qq.com>
2019-12-10 13:48:23 +0800

    init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# compress-unpack
+
+### compress-unpack介绍
+compress-unpack是文件压缩、压缩工具类，包括文件、文件夹的压缩，可以支持压缩为如下格式：
+- rar
+- zip
+- tar.gz
+
+### rar格式的压缩与解压
+### zip格式的压缩与解压
+### tar.gz格式的压缩与解压
---
+++ src/main/java/com/h2t/study/CompressUnpackApplication.java
@@ -0,0 +1,13 @@
+package com.h2t.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompressUnpackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompressUnpackApplication.class, args);
+    }
+
+}
